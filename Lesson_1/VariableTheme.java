d5f030579116dccf4c069cf7da688e9c349b302e
Yevhenii Stryhnaliuk <uss.jeka@gmail.com>
2022-09-16 23:54:39 +0200

    fix code syle 0.2

--- Lesson_1/VariableTheme.java
+++ Lesson_1/VariableTheme.java
@@ -43,13 +43,14 @@ public class VariableTheme { //здравия желаю
 
         System.out.println("\nNo_5. Перестановка значений переменных.");
         int num1 = 2, num2 = 5, num3;
-        System.out.println("Через промежуточную переменную: num1 = " + num1 + " num2 = " + num2);
+        System.out.println("Через промежуточную переменную: num1 = " + num1 +
+                 " num2 = " + num2);
         num3 = num1;
         num1 = num2;
         num2 = num3;
         System.out.println("Завершено: num1 = " + num1 + " num2 = " + num2);
-        System.out.println("\nС помощью арифметических операций: num1 = "
-            + num1 + " num2 = " + num2);
+        System.out.println("\nС помощью арифметических операций: num1 = " +
+                num1 + " num2 = " + num2);
         num1 += num2; 
         num2 = num1 - num2;
         num1 -= num2;
