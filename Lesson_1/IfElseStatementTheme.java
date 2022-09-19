0ff53b8567619061e038165c9d51f4f533b770ea
Yevhenii Stryhnaliuk <uss.jeka@gmail.com>
2022-09-19 16:27:53 +0200

    Code Style if-else fix

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -3,14 +3,14 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("1. Перевод псевдокода на язык Java");
         int age = 22;
-        char gender = 'M';
+        boolean gender = true; //1 муж; 0 жен.
         double height = 1.86;
         if (age > 20) {
             System.out.println("Можете покупать алкоголь");
         } else {
             System.out.println("Алкоголь продается лицам исключительно старше 21 года");
         }
-        if (!(Character.toLowerCase(gender) == 'm')) {
+        if (!gender) {
             System.out.println("Хватит бухать начни вязать");
         } else {
             System.out.println("Хватит бухать займись спортом");
@@ -27,14 +27,14 @@ public class IfElseStatementTheme {
         } else if (firstCharName == 'I') {
             System.out.println("Тебя могут звать Иван");
         } else {
-                System.out.println("Может быть ты Ваня? Вантеллион?");
+            System.out.println("Может быть ты Ваня? Вантеллион?");
         }
 
         System.out.println("\n2. Поиск max и min числа");
         int num1 = 282, num2 = 280;
-        if (num1<num2) {
+        if (num1 < num2) {
             System.out.println("Min numbers = " + num1 + "; " + "Max numbers = " + num2);
-        } else if (num1>num2) {
+        } else if (num1 > num2) {
             System.out.println("Min numbers = " + num2 + "; " + "Max numbers = " + num1);
         } else {
             System.out.println(num1 + " = " + num2 );
@@ -53,8 +53,10 @@ public class IfElseStatementTheme {
             }
             if (num1 > 0) {
                 System.out.println("и это положительное число!\n");
-            } else {
+            } else if (num1 < 0) {
                 System.out.println("и это отрицательное число!\n");
+            } else {
+                System.out.println("число равно нулю!\n");
             }
         }
 
@@ -62,23 +64,24 @@ public class IfElseStatementTheme {
         //Используем две существующие переменные добавим флаги для удобства
         num1 = 324;
         num2 = 124;
-        boolean onesFlag = (num1 % 10) == (num2 % 10);
-        boolean dozensFlag = ((num1 % 100) / 10) == ((num2 % 100) / 10);
-        boolean hundredsFlag = (num1 / 100) == (num2 / 100);
+        boolean equalsOnes = (num1 % 10) == (num2 % 10);
+        boolean equalsDozens = ((num1 % 100) / 10) == ((num2 % 100) / 10);
+        boolean equalsHundreds = (num1 / 100) == (num2 / 100);
         System.out.println("Исходные числа: No1 = " + num1 + " No2 = " + num2);
-        // конкретно в данной ситуации просто комбинируя условия через && || кода получится больше тк нужно составить табл истинности 3х3
+        /* конкретно в данной ситуации просто комбинируя условия через && ||
+        * кода получится больше тк нужно составить табл истинности 3х3 */
         if (num1 != num2) { 
-            if (hundredsFlag) {
+            if (equalsHundreds) {
                 System.out.println("I разрядные цифры одинаковы и = " + (num1 / 100));
             } else {
                 System.out.println("I разрядные цифры не равны!");
             }
-            if (dozensFlag) {
+            if (equalsDozens) {
                 System.out.println("II разрядные цифры одинаковы и = " + ((num1 % 100) / 10));
             } else {
                 System.out.println("II разрядные цифры не равны!");
             }
-            if (onesFlag) {
+            if (equalsOnes) {
                 System.out.println("III разрядные цифры одинаковы и = " + (num1 % 10));
             } else {
                 System.out.println("III разрядные цифры не равны!");
@@ -88,30 +91,21 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\n5. Определение буквы, числа или символа по их коду");
-        char ch = '\u0057';
-        int decCode = (int) ch; 
+        char ch = '\u0057'; 
         /*
          *как по мне методы Integer и методы для проверки char стоило бы использовать, т.к. :
          *они не выходят за тему примитивные типы данных.
          */
-        System.out.println("Преобразование HEX в DEC завершено, код: " + decCode);
-        if (ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' || ch == '7' ||
-                ch == '8' || ch == '9' || ch == '0') {
-            System.out.println("Код " + decCode + " - " + "Является цифрой " + ch);
+        System.out.println("Преобразование HEX в DEC завершено, код: " + (int) ch);
+        if ((int) ch >= 48 && (int) ch <= 57) {
+            System.out.println("Код " + (int) ch + " - " + "Является цифрой " + ch);
         } else {
-            if (ch == 'Q' || ch == 'W' || ch == 'E' || ch == 'R' || ch == 'T' || ch == 'Y' || ch == 'U' ||
-                    ch == 'I' || ch == 'O' || ch == 'P' || ch == 'A' || ch == 'S' || ch == 'D' || 
-                    ch == 'F' || ch == 'G' || ch == 'H' || ch == 'J' || ch == 'K' || ch == 'L' ||
-                    ch == 'Z' ||ch == 'X' || ch == 'C' || ch == 'V' || ch == 'B' || ch == 'N' ||
-                    ch == 'M') {
-                System.out.println("Код " + decCode + ", соответствует большой литере - " + ch); 
-            } else if (ch == 'q' || ch == 'w' || ch == 'e' || ch == 'r' || ch == 't' || ch == 'y' ||
-                    ch == 'u' || ch == 'i' || ch == 'o' || ch == 'p' || ch == 'a' || ch == 's' || ch == 'd' || 
-                    ch == 'f' || ch == 'g' || ch == 'h' || ch == 'j' || ch == 'k' || ch == 'l' || ch == 'z' ||
-                    ch == 'x' || ch == 'c' || ch == 'v' || ch == 'b' || ch == 'n' || ch == 'm') {
-                System.out.println("Код " + decCode + ", соответствует малой литере - " + ch); 
+            if ((int) ch >= 65 && (int) ch <= 90) {
+                System.out.println("Код " + (int) ch + ", соответствует большой литере - " + ch); 
+            } else if ((int) ch >= 97 && (int) ch <= 122 ) {
+                System.out.println("Код " + (int) ch + ", соответствует малой литере - " + ch); 
             } else {
-                System.out.println("Код " + decCode + ", - не является литерой либо цифрой, это символ " + ch);
+                System.out.println("Код " + (int) ch + ", - не является литерой либо цифрой, это символ " + ch);
             }
         }
 
@@ -169,36 +163,37 @@ public class IfElseStatementTheme {
 
         System.out.println("\n9. Подсчет количества банкнот");
         int requiredPay = 567;
-        int $1billLimit = 929, $10billLimit = 5, $100billLimit = 340;
-        int $1cash = 0, $10cash = 0, $100cash = 0;
+        int billLimit1 = 929, billLimit10 = 5, billLimit100 = 340;
+        int dollarCash1sh = 0, dollarCash10 = 0, dollarCash100 = 0;
+        int requiredDozens = (requiredPay % 100) / 10;
         System.out.println("Добро пожаловать!\nВ наличии купюры номиналом" + "\n100$ 10$ 1$\n" + 
-                $100billLimit + " " + $10billLimit + " " + $1billLimit + "\n Требуемая сумма = " + requiredPay); 
+                billLimit100 + " " + billLimit10 + " " + billLimit1 + "\n Требуемая сумма = " + requiredPay);
         if ((requiredPay > 0)) { 
-            if ($1billLimit > (requiredPay % 10)) {
-                $1cash += (requiredPay % 10);
-                $1billLimit -= $1cash;
-                if ($10billLimit > ((requiredPay % 100) / 10)) {
-                    $10cash += ((requiredPay % 100) / 10);
-                    $10billLimit -= $10cash;
+            if (billLimit1 > (requiredPay % 10)) {
+                dollarCash1sh += (requiredPay % 10);
+                billLimit1 -= dollarCash1sh;
+                if (billLimit10 > requiredDozens) {
+                    dollarCash10 += requiredDozens;
+                    billLimit10 -= dollarCash10;
                 } else {
-                    int add$1 = ((requiredPay % 100) / 10) - $10billLimit; //Локальная удобна, т.к. когда нибудь ее заберет gc
-                    $1cash += add$1;
-                    $10cash = ((requiredPay % 100) / 10) - add$1;
-                    $1billLimit -= add$1;
-                    $10billLimit -= $10cash;
+                    int reservedDollar = requiredDozens - billLimit10; //Локальная удобна, т.к. когда нибудь ее заберет gc
+                    dollarCash1sh += reservedDollar;
+                    dollarCash10 = requiredDozens - reservedDollar;
+                    billLimit1 -= reservedDollar;
+                    billLimit10 -= dollarCash10;
                 }
-                if ((requiredPay / 100) < $100billLimit) {
-                    $100cash += requiredPay / 100;
-                    $100billLimit -= $100cash;
+                if ((requiredPay / 100) < billLimit100) {
+                    dollarCash100 += requiredPay / 100;
+                    billLimit100 -= dollarCash100;
                 }
             }
         }
         System.out.println("Итоговая сумма: " + requiredPay + "\nКупюр номиналом выдано\n" + 
-                "100$" + " - " + $100cash + " шт. " +
-                "10$" + " - " + $10cash + " шт. " +
-                "1$" + " - " + $1cash + " шт. " + "\nХорошего дня");
+                "100$" + " - " + dollarCash100 + " шт. " +
+                "10$" + " - " + dollarCash10 + " шт. " +
+                "1$" + " - " + dollarCash1sh + " шт. " + "\nХорошего дня");
         requiredPay = 0;
         System.out.println("\nДобро пожаловать!\nВ наличии купюры номиналом" + "\n100$ 10$ 1$\n" + 
-                $100billLimit + " " + $10billLimit + " " + " " +$1billLimit);
+                billLimit100 + " " + billLimit10 + " " + " " +billLimit1);
     }
 }
