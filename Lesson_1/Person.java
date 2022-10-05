911d63456d8181728c7083c9e9d679e596a8e210
Yevhenii Stryhnaliuk <uss.jeka@gmail.com>
2022-10-05 23:14:58 +0200

    Lesson2 Implemented Class Person (HW)

---
+++ Lesson_1/Person.java
@@ -0,0 +1,27 @@
+class Person {
+    String name = "John";
+    char gender = 'M';
+    int age = 23;
+    float weight = 88.6f;
+    float height = 1.82f;
+
+    void stay() {
+        System.out.println("- I'm waiting!");
+    }
+    void move() {
+        System.out.println("move");
+    }
+    void run() {
+        System.out.println("run");
+    }
+    void jump() {
+        System.out.println("jump");
+    }
+
+    String say() {
+        return "Hi!";
+    }
+    void learningJava() {
+        System.out.println("Start Learning");
+    }
+}
