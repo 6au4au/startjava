4a772c3d156cdb88cb86c068f3c5d20d78127823
Yevhenii Stryhnaliuk <uss.jeka@gmail.com>
2022-10-09 18:42:50 +0200

    Update Wolf class and Wolf Test: get\set and validation

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,19 +1,100 @@
 class Wolf {
-    String name;
-    String animalColor;
-    char gender;
-    int age;
-    float weight;
-    int speed;
-    boolean movement;
+    private String name;
+    private String animalColor;
+    private char gender;
+    private int age;
+    private float weight;
+    private int speed;
+    private boolean movement;
 
-    void stay() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.trim().length() >= 2) {
+            this.name = name.trim();
+        } else {
+            System.out.println("Err: The name of the wolf cannot be shorter than 2 characters or contain spaces!"); 
+        }
+    }
+
+    public String getAnimalColor() {
+        return animalColor;
+    }
+
+    public void setAnimalColor(String animalColor) {
+        switch (animalColor.trim().toLowerCase()) {
+            case "grey":
+            case "brown":
+            case "white":
+                this.animalColor = animalColor.trim().toLowerCase();
+                break;
+            default:
+                System.out.println("Err: not valid wolf color, List:\n grey, brown, white!");
+        }
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if (gender == 'M' || gender == 'F') {
+            this.gender = gender;
+        } else {
+            System.out.println("Err: wolf can only be 'M'ale or 'F'emale gender!");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 8 || age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Err: new wolf cannot be older than 8 years, and under 1 year old!");
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if (weight >= 22.0f || weight <= 70.0f) {
+            this.weight = weight;
+        } else {
+            System.out.println("Err: a wolf cannot weigh less than 22 kg or more than 70 kg!");
+        }
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+    // этот сеттер под большим вопросом но в зависимости от факторов в моей симуляции
+    // скорость волка также может изменятся потому я его сделал.
+    public void setSpeed(int speed) {
+        if (speed >= 0 || speed <= 53) {
+            this.speed = speed;
+        } else {
+            System.out.println("Err: Wolf speed cannot be less than 0 and more than 53 (km/h)!");
+        }
+    }
+    //в пару этому геттеру сеттер не нужен так-как изменения состояния прописано в методах.
+    public boolean movement() {
+        return movement;
+    }
+
+    public void stay() {
         System.out.println(name + ": sits.");
         speed = 0;
         movement = false;
     }
 
-    void move(float distanceInMeters) {
+    public void move(float distanceInMeters) {
         movement = true;
         speed += 8;
         String movementType;
@@ -29,21 +110,22 @@ class Wolf {
         movement = false;
     }
 
-    void run(float distanceInMeters) {
+    public void run(float distanceInMeters) {
         movement = true;
         speed += 45;
         move(distanceInMeters);
     }
 
-    void voice(int amountHowling) {
+    public void voice(int amountHowling) {
         for (; amountHowling > 0; amountHowling--) {
             System.out.println(name + ": howls");
         }
     }
 
-    void hunt(float targetDistance) {
+    public void hunt(float targetDistance) {
         System.out.println(name + ": hit the trail!");
         run(targetDistance);
+        System.out.println(name + ": attacked!");
         double huntResult = Math.random();
         if (huntResult <= 0.3) {
             System.out.println("The hunt failed!");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,19 +2,24 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolf1 = new Wolf();
-        wolf1.animalColor = "White";
-        wolf1.name = "Herald";
-        wolf1.gender = 'M';
-        wolf1.age = 10;
-        wolf1.weight = 75.5f;
+        wolf1.setName("Herald");
+        wolf1.setAnimalColor("White");
+        wolf1.setGender('M');
+        wolf1.setAge(8);
+        wolf1.setWeight(45.5f);
 
-        System.out.println("WOLF SIMULATION");
-        System.out.println(wolf1.name + " goes to observation point in 20m");
+        System.out.println("WOLF CREATED INFO: ");
+        System.out.println("Name: " + wolf1.getName() + "\nGender: " + wolf1.getGender() + "\nAnimal Color: " 
+                + wolf1.getAnimalColor() + "\nAnimal Age: " + wolf1.getAge() + "\nAnimal Weight: " 
+                + wolf1.getWeight());
+        
+        System.out.println("\nWOLF SIMULATION");
+        System.out.println(wolf1.getName() + " goes to observation point in 20m");
         wolf1.move(20.0f);
         wolf1.stay();
         wolf1.voice(2);
         //метод охоты вызывает из себя метод бега, потому я отдельно его не вызывал
-        System.out.println("\n" + wolf1.name + ": detected hare 40m");
+        System.out.println("\n" + wolf1.getName() + ": detected hare 40m");
         wolf1.hunt(40.0f);
     }
 }
