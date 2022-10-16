42aa7d4668aa2a17e4442dc22aae45d97de4f64c
Yevhenii Stryhnaliuk <uss.jeka@gmail.com>
2022-10-16 14:37:20 +0200

    fix Calculator and CalculatorTest

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,29 +1,35 @@
+import java.util.Scanner;
 class Calculator {
     //оператор (+, -, *, /, ^, %)
-    private int a, b, result;
-    private char ch;
+    private int a, b;
+    private char matOperation;
     private String err1 = "ERR N1: Поддерживаются только целые положительные числа!";
     private String err2 = "ERR N2: Поддерживаются только эти математические операции: (+, -, *, /, ^, %)";
     public int getA() {
         return a;
     }
 
-    public void setA(int youVariables) {
-        a = youVariables;
+    public void setA(int a) {
+        this.a = a;
     }
 
     public int getB() {
         return b;
     }
 
-    public void setB(int youVariables) {
-        b = youVariables;
+    public void setB(int b) {
+        this.b = b;
     }
 
-    public void setCh(String youChar) {
+    public void reset() {
+        a = 0;
+        b = 0;
+    }
+
+    public void setMatOperation(Scanner scanner) {
         do {
-            ch = youChar.trim().charAt(0);
-            switch (ch) {
+            matOperation = scanner.nextLine().trim().charAt(0);
+            switch (matOperation) {
                 case '+':
                 case '-':
                 case '*':
@@ -39,8 +45,13 @@ class Calculator {
         } while (true);
     }
 
-    public void setResult() {
-        switch (ch) {
+    public char getMatOperation() {
+        return matOperation;
+    }
+
+    public int showResult() {
+        int result = 0;
+        switch (matOperation) {
             case '+':
                 result = a + b;
                 break;
@@ -63,13 +74,6 @@ class Calculator {
                 result = a % b;
                 break;
         }
-    }
-
-    public int getResult() {
         return result;
-    }
-
-    public char getCh() {
-        return ch;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,46 +2,46 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     private static boolean included = true;
-    private static boolean questionWindowsOperation = false;
     //ошибка которая связанна обработкой ответа о продолжении/завершении работы: 
     private String err3 = "ERR N3: Поддерживается только ввод: YES или NO";
 
-    public void userWantsToContinue(String choice) {
+    public void wantsToContinue(Scanner scanner, Calculator calculator) {
+        String choice = scanner.nextLine();
         if (choice.equalsIgnoreCase("no")) {
             System.out.println("Power OFF.");
-            questionWindowsOperation = true;
             included = false;
         } else if (choice.equalsIgnoreCase("yes")) {
-            questionWindowsOperation = true;
+            calculator.reset();
+            included = true;
         } else {
             System.out.println(err3);
         }
     }
 
     public static void main(String[] args) {
-        CalculatorTest userModule = new CalculatorTest();
-        Calculator calculate = new Calculator();
+        CalculatorTest calculatorTest = new CalculatorTest();
+        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         do {
-            questionWindowsOperation = false;
-            System.out.print("Введите первое число: ");
-            calculate.setA(scanner.nextInt());
-            System.out.print("\nУкажите знак математической операции: (+, -, *, /, ^, %): ");
-            scanner.nextLine();
-            calculate.setCh(scanner.nextLine());
-            System.out.print("\nВведите второе число: ");
-            calculate.setB(scanner.nextInt());
-            System.out.println();
-            System.out.print("Результат: ");
-            calculate.setResult();
-            System.out.println(calculate.getA() + " " + calculate.getCh() + " " + calculate.getB() + " = " + calculate.getResult());
-            //Очистка сканнера перед формой:
-            scanner.nextLine();
-            do {
+            if (calculator.showResult() == 0) {
+                System.out.print("Введите первое число: ");
+                calculator.setA(scanner.nextInt());
+                System.out.print("\nУкажите знак математической операции: (+, -, *, /, ^, %): ");
+                //очистка сканнера:
+                scanner.nextLine();
+                calculator.setMatOperation(scanner);
+                System.out.print("\nВведите второе число: ");
+                calculator.setB(scanner.nextInt());
+                System.out.println();
+                System.out.print("Результат: ");
+                System.out.println(calculator.getA() + " " + calculator.getMatOperation() + " " + calculator.getB() + 
+                        " = " + calculator.showResult());
+                //очистка сканнера:
+                scanner.nextLine();
+            } else {
                 System.out.print("Желаете продолжить (YES/NO): ");
-                String choice = scanner.nextLine().trim();
-                userModule.userWantsToContinue(choice);
-            } while (!questionWindowsOperation);
+                calculatorTest.wantsToContinue(scanner, calculator);
+            }
         } while (included);
     }
 }
