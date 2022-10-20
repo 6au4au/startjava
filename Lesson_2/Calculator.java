a01cbff73586baef4ced2e4032984b674f42a3f0
Yevhenii Stryhnaliuk <uss.jeka@gmail.com>
2022-10-20 17:03:46 +0200

    Refactoring Calculator.java

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,66 +1,33 @@
-import java.util.Scanner;
-
 class Calculator {
     //оператор (+, -, *, /, ^, %)
     private int a, b;
     private char matOperation;
-    private String err1 = "ERR N1: Поддерживаются только целые положительные числа! ";
-    private String err2 = "ERR N2: Поддерживаются только эти математические операции: (+, -, *, /, ^, %)";
     
     public int getA() {
         return a;
     }
 
-    public void setA(Scanner scanner) {
-        a = setDimens(scanner);
+    public void setA(int a) {
+        this.a = a;
     }
 
     public int getB() {
         return b;
     }
 
-    public void setB(Scanner scanner) {
-       b = setDimens(scanner);
+    public void setB(int b) {
+       this.b = b;
     }
 
-    private int setDimens(Scanner scanner) {
-        int integer = 0;
-        while (true) {
-            if (scanner.hasNextInt()) {
-                integer = scanner.nextInt();
-                break;
-            } else {
-                System.out.println(err1);
-            }
-            scanner.nextLine();
-        }
-        return integer;
-    }
-
-    public void setMatOperation(Scanner scanner) {
-        do {
-            matOperation = scanner.nextLine().trim().charAt(0);
-            switch (matOperation) {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '^':
-                case '%':
-                    break;
-                default:
-                    System.out.println(err2);
-                    continue;
-            }
-            break;
-        } while (true);
+    public void setMatOperation(char matOperation) {
+        this.matOperation = matOperation;
     }
 
     public char getMatOperation() {
         return matOperation;
     }
 
-    public int getResult() {
+    public int calculate() {
         switch (matOperation) {
             case '+':
                 return a + b;
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -15,16 +15,16 @@ public class CalculatorTest {
                 //обращаемся к static boolean и переключаем 
                 didItWork = true;
                 System.out.print("Введите первое число: ");
-                calculator.setA(scanner);
+                calculator.setA(calculatorTest.validateNumber(scanner));
                 System.out.print("\nУкажите знак математической операции: (+, -, *, /, ^, %): ");
                 //очистка сканнера:
                 scanner.nextLine();
-                calculator.setMatOperation(scanner);
+                calculator.setMatOperation(calculatorTest.validateMatOperation(scanner));
                 System.out.print("\nВведите второе число: ");
-                calculator.setB(scanner);
+                calculator.setB(calculatorTest.validateNumber(scanner));
                 System.out.print("\nРезультат: ");
                 System.out.println(calculator.getA() + " " + calculator.getMatOperation() + " " + calculator.getB() + 
-                        " = " + calculator.getResult());
+                        " = " + calculator.calculate());
                 scanner.nextLine();
             }
             System.out.println("Желаете продолжить? YES / NO");
@@ -42,6 +42,41 @@ public class CalculatorTest {
             default:
                 System.out.println("Ошибка, допустимые варианты только YES или NO!");
                 return true;
+        }
+    }
+
+    private int validateNumber(Scanner scanner) {
+        int result;
+        while (true) {
+            if (!scanner.hasNextInt()) {
+                System.out.println("Недопустимый формат ввода, поддерживаются только целые положительные числа!");
+                scanner.next();
+                continue;
+            }
+            result = scanner.nextInt();
+            if (result == 0) {
+                System.out.println("Операции с 0 бессмысленны!");
+                continue;
+            }
+            break;
+        }
+        return result;
+    }
+
+    private char validateMatOperation(Scanner scanner) {
+        while (true) {
+            char matOperation = scanner.nextLine().trim().charAt(0);
+            switch (matOperation) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                case '%':
+                    return matOperation;
+                default:
+                    System.out.println("Поддерживаются только эти математические операции: (+, -, *, /, ^, %)");
+            }
         }
     }
 }
